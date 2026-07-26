2c11e017e234c731d042b9c9823bd1fbeab23b02
Piyush Prasad <piyushprasad0521@gmail.com>
2026-07-26 14:48:21 +0530

    size of Compose Button

--- src/app/components/ComposeMessage/page.jsx
+++ src/app/components/ComposeMessage/page.jsx
@@ -7,38 +7,8 @@ const ComposeButton = () => {
   return (
     <Link href="/form">
       <button
-        className="
-          fixed
-          bottom-30
-          right-5
-          z-10
-          md:left-90
-          md:bottom-20
-          lg:left-110
-          lg:bottom-20
-
-          w-14
-          h-14
-
-          rounded-full
-          bg-[#25D366]
-          text-white
-
-          flex
-          items-center
-          justify-center
-
-          shadow-2xl
-
-          hover:bg-[#1ebe5d]
-          hover:scale-110
-          active:scale-95
-
-          transition-all
-          duration-300
-        "
-      >
-        <HiOutlinePencilSquare size={28} />
+        className="fixed bottom-30 right-5  z-10  md:left-90  md:bottom-20  lg:left-110  lg:bottom-20  w-15 h-15 rounded-full bg-[#25D366] text-white flex items-center justify-center shadow-2xl hover:bg-[#1ebe5d] hover:scale-110 active:scale-95 transition-all duration-300">
+        <HiOutlinePencilSquare size={30} />
       </button>
     </Link>
   );
